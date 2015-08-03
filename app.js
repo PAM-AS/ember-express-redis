0388c62965f1824816706bf23bf8e699627127ab
thomassnielsen <me@thomassnielsen.com>
2015-08-03 20:19:40 +0200

    Add optional password

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var app = require('express')();
 
 var appname = process.env.APP_NAME || "unnamed-app";
+var password = process.env.REDIS_PASSWORD || undefined;
 
 var nodeEmberCliDeployRedis = require('node-ember-cli-deploy-redis');
 app.use('/*', nodeEmberCliDeployRedis(appname, {
   host: 'redis',
   port: 6379,
-  database: 0
+  database: 0,
+  password: password
 }));
 
 module.exports = app;
