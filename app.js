074098ab65e3cf2dabc526fd1520b03823c68004
thomassnielsen <me@thomassnielsen.com>
2015-08-03 13:19:05 +0200

    Use name from environment variable if available

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var app = require('express')();
 
+var appname = process.env.APP_NAME || "unnamed-app";
+
 var nodeEmberCliDeployRedis = require('node-ember-cli-deploy-redis');
-app.use('/*', nodeEmberCliDeployRedis('myapp', {
+app.use('/*', nodeEmberCliDeployRedis(appname, {
   host: 'redis',
   port: 6379,
   database: 0
